ce55dad1745ddcd6efd8c5e1b2585a3fb78b1eb5
Ridwan <ridwanbanjar1122@gmail.com>
2024-02-21 08:08:33 +0700

    perbarui proses input pengguna

--- petugas/proses/proses_tambah_buku.php
+++ petugas/proses/proses_tambah_buku.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 include '../../koneksi.php';
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $perpustakaan = $_POST['perpustakaan'];
     $judul = $_POST['judul'];
     $penulis = $_POST['penulis'];
@@ -12,34 +12,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $cover = $_FILES["cover"];
 
     // Lakukan validasi dan penambahan buku
-    if (!empty($judul) && !empty($penulis) && !empty($penerbit) && !empty($tahun_terbit) && !empty($sinopsis) && !empty($kategori)) {
-        // Query untuk menambahkan buku ke dalam database
-         // Sesuai dengan logika Anda
- // Mengunggah foto
-        $targetDir = "";
-        $targetFile = $targetDir . basename($_FILES["cover"]["name"]);
-        move_uploaded_file($_FILES["cover"]["tmp_name"], $targetFile);
-        
+    if (!empty($judul) && !empty($penulis) && !empty($penerbit) && !empty($tahun_terbit) && !empty($kategori)) {
+        // Memeriksa ekstensi file
+        $allowedExtensions = array("jpg", "jpeg", "png", "svg");
+        $fileExtension = strtolower(pathinfo($cover["name"], PATHINFO_EXTENSION));
 
-            $add_book_query = "INSERT INTO `buku` (perpus_id, foto, judul, penulis, penerbit, tahun_terbit, sinopsis, kategori_id)
-                               VALUES ('$perpustakaan', '$targetFile', '$judul', '$penulis', '$penerbit', '$tahun_terbit', '$sinopsis', '$kategori')";
+        if (in_array($fileExtension, $allowedExtensions)) {
+            // Mengunggah foto jika ekstensinya valid
+            $targetDir = "../ ../asset/";
+            $targetFileName = uniqid() . '.' . $fileExtension; // Menyimpan nama unik file untuk menghindari tumpang tindih nama
+            $targetFile = $targetDir . $targetFileName;
 
-            // Eksekusi query dan tampilkan pesan sukses atau error
-            if (mysqli_query($koneksi, $add_book_query)) {
-                $success_message = "Buku berhasil ditambahkan.";
-                // Redirect to the desired page after successful insertion
-                header("location:../buku.php");
-                
-                exit; // Exit the script after redirection
+            if (move_uploaded_file($cover["tmp_name"], $targetFile)) {
+                // Query untuk menambahkan buku ke dalam database
+                $add_book_query = "INSERT INTO `buku` (perpus_id, foto, judul, penulis, penerbit, tahun_terbit, sinopsis, kategori_id)
+                               VALUES ('$perpustakaan', '$targetFileName', '$judul', '$penulis', '$penerbit', '$tahun_terbit', '$sinopsis', '$kategori')";
+
+                // Eksekusi query dan tampilkan pesan sukses atau error
+                if (mysqli_query($koneksi, $add_book_query)) {
+                    $success_message = "Buku berhasil ditambahkan.";
+                    // Redirect to the desired page after successful insertion
+                    header("location:../buku.php");
+                    exit; // Exit the script after redirection
+                } else {
+                    $error_message = "Error: " . mysqli_error($koneksi);
+                }
             } else {
-                $error_message = "Error: " . mysqli_error($koneksi);
+                $error_message = "Maaf, terjadi kesalahan saat mengunggah gambar.";
             }
         } else {
-            $error_message = "Maaf, terjadi kesalahan saat menyimpan gambar.";
+            $error_message = "Hanya file dengan ekstensi .jpg, .jpeg, .png, atau .svg yang diperbolehkan.";
         }
     } else {
         $error_message = "Semua kolom harus diisi.";
     }
+
     // Display error message if any
     if (isset($error_message)) {
         echo $error_message;
@@ -47,5 +54,5 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     // Close the database connection
     mysqli_close($koneksi);
-
+}
 ?>
--- proses/proses_input_pengguna.php
+++ proses/proses_input_pengguna.php
@@ -4,19 +4,53 @@ include '../koneksi.php';
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $perpustakaan = $_POST['perpustakaan'];
     $username = $_POST['username'];
+    $email = $_POST['email'];
     $password = $_POST['password'];
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-    $email = $_POST['email'];
     $namalengkap = $_POST['nama_lengkap'];
     $alamat = $_POST['alamat'];
     $role = $_POST['role'];
 
-    $sql = "INSERT INTO user (perpus_id, username, password, email, nama_lengkap, alamat, role) VALUES ('$perpustakaan', '$username', '$hashed_password', '$email', '$namalengkap', '$alamat', '$role')";
+    // Inisialisasi array untuk menyimpan pesan kesalahan
+    $errors = array();
 
-    if (mysqli_query($koneksi, $sql)) {
-        header("location:../admin/pengguna.php");
+    // Cek apakah username sudah ada
+    $cek_username = mysqli_query($koneksi, "SELECT * FROM user WHERE username='$username'");
+    if(mysqli_num_rows($cek_username) > 0){
+        // Jika username sudah ada, tambahkan pesan kesalahan ke array
+        $errors[] = "Username sudah digunakan. Silakan pilih username lain.";
+    }
+
+    // Cek apakah email sudah ada
+    $cek_email = mysqli_query($koneksi, "SELECT * FROM user WHERE email='$email'");
+    if(mysqli_num_rows($cek_email) > 0){
+        // Jika email sudah ada, tambahkan pesan kesalahan ke array
+        $errors[] = "Email sudah digunakan. Silakan gunakan email lain.";
+    }
+
+    // Jika array errors tidak kosong, berarti ada kesalahan
+    if(!empty($errors)) {
+        // Gabungkan pesan kesalahan menjadi satu string dengan menggunakan fungsi implode()
+        $pesan_kesalahan = implode("\\n", $errors);
+        // Simpan nilai-nilai input ke dalam session
+        session_start();
+        $_SESSION['perpustakaan'] = $perpustakaan;
+        $_SESSION['username'] = $username;
+        $_SESSION['email'] = $email;
+        $_SESSION['nama_lengkap'] = $namalengkap;
+        $_SESSION['alamat'] = $alamat;
+        $_SESSION['role'] = $role;
+        // Tampilkan pesan kesalahan menggunakan JavaScript alert
+        echo "<script>alert('$pesan_kesalahan'); window.location.href = '../admin/input/registrasi_pengguna.php';</script>";
     } else {
-        echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+        // Jika tidak ada kesalahan, lanjutkan proses register
+        $sql = "INSERT INTO user (perpus_id, username, password, email, nama_lengkap, alamat, role) VALUES ('$perpustakaan', '$username', '$hashed_password', '$email', '$namalengkap', '$alamat', '$role')";
+
+        if (mysqli_query($koneksi, $sql)) {
+            header("location:../admin/pengguna.php");
+        } else {
+            echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+        }
     }
 
     // Tutup koneksi ke database
